b1544a7612c5218727fe23ffb1df48f4d0a31399
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-25 12:26:13 +0000

    License, maintainers, descriptoin

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'RepeatMasker'
-maintainer 'The Authors'
+maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'all_rights'
+license 'apache2'
 description 'Installs/Configures RepeatMasker'
-long_description 'Installs/Configures RepeatMasker'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
 depends 'build-essential'
