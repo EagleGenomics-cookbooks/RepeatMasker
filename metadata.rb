b689068aa40c11997e5c69c3b276c12fa6f692f7
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-27 10:54:59 +0000

    Standardising license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'RepeatMasker'
 maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'apache2'
+license 'Apache v2.0'
 description 'Installs/Configures RepeatMasker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
