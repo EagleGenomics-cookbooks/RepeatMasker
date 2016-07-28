560710a04dc6ca7db69fc313a1db91679a512bd7
Nick James <nick.james@eaglegenomics.com>
2016-07-28 14:25:02 +0100

    fixed rubocop / foodcritic

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,10 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures RepeatMasker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
+
+source_url 'https://github.com/EagleGenomics-cookbooks/RepeatMasker'
+issues_url 'https://github.com/EagleGenomics-cookbooks/RepeatMasker/issues'
 
 depends 'build-essential'
 depends 'magic_shell'
